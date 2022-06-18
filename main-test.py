3c0bea9ecd2c00fdffdbb3cfa4a18c94fb098f36
danielbatubenga <daniel.batubenga@gmail.com>
2022-06-19 00:39:48 +0100

    primeiro commit

---
+++ main-test.py
@@ -0,0 +1,6 @@
+# Importe a função Add e confirme que ela funciona corretamente. 
+from main import Add def TestAdd():
+         assert Add(2,3) == 5 
+        print( "Adicionar função funciona corretamente" ) if __name__ == '__main__' : 
+        TestAdd()
+
