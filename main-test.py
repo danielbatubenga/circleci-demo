f1810817b6ce9f0eda264a4b7edcd584bfc95b3f
danielbatubenga <daniel.batubenga@gmail.com>
2022-06-19 01:03:37 +0100

    Adds test to make sure 5 + 5 is equal to 10

--- main-test.py
+++ main-test.py
@@ -1,6 +1,6 @@
-# Importe a função Add e confirme que ela funciona corretamente. 
+i# Importe a função Add e confirme que ela funciona corretamente. 
 from main import Add def TestAdd():
          assert Add(2,3) == 5 
         print( "Adicionar função funciona corretamente" ) if __name__ == '__main__' : 
-        TestAdd()
+        TestAdd(assert Adicionar(5,5) == 10)
 
